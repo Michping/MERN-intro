536773398c98c5825e06f12134da272eb4972ee1
michael nikolaisen <michaelhalnik@gmail.com>
2023-09-23 13:36:14 +0200

    login side ferdig

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterOgLogin'
+import { FormRow, Logo } from '../components'
 
 const Login = () => {
   return (
-    <div>
-      <h1>Login</h1>
-      <Link to="/register">Registrerings side</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Logg inn</h4>
+        <FormRow type="email" name="email" defaultValue="johnsmith@mail.com" />
+        <FormRow type="password" name="password" labelText="passord" defaultValue="passord123" />
+        <button type='submit' className='submit-btn'>Logg inn</button>
+        <button type='button' className='submit-btn'>Utforsk appen</button>
+        <p>
+          Har du ikke bruker?
+          <Link to="/register" className='login-btn'>
+            Registrer
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
