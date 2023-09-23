e69ba3d6b6a641a646029a8a5dd701e01e824d4f
michael nikolaisen <michaelhalnik@gmail.com>
2023-09-23 13:25:15 +0200

    registrering side ferdig

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Wrapper from '../assets/wrappers/RegisterOgLogin'
+import { FormRow, Logo } from '../components'
 
 const Register = () => {
   return (
-    <div>
-      <h1>Registrer</h1>
-      <Link to="/login">Login side</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Registrer</h4>
+        <FormRow type="text" name="name" labelText="navn" defaultValue="john" />
+        <FormRow type="text" name="lastName" labelText="etternavn" defaultValue="smith" />
+        <FormRow type="text" name="location" labelText="sted" defaultValue="norge" />
+        <FormRow type="text" name="email" defaultValue="johnsmith@mail.com" />
+        <FormRow type="password" name="password" labelText="passord" defaultValue="passord123" />
+        <button type='submit' className='submit-btn'>
+          registrer
+        </button>
+        <p>
+          Allerede medlem?
+          <Link to="/login" className='login-btn'>
+            Logg inn
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   )
 }
 
